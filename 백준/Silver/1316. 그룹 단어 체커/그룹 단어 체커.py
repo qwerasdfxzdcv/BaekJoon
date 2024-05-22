84d268bf1b9d05fa5623a879a150c1556642fb80
qwerasdfxzdcv <104199005+qwerasdfxzdcv@users.noreply.github.com>
2024-05-23 00:54:43 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = n
+for _ in range(n) :
+    word = input()
+    for i in range(len(word)-1) :
+        if word[i] == word[i+1] :
+            pass
+        elif word[i] in word[i+1:] :
+            a -= 1
+            break
+print(a)
