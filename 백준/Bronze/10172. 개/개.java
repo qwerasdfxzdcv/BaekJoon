05b1c6ac00e3f3397571556aa59d8d1832ed1891
qwerasdfxzdcv <104199005+qwerasdfxzdcv@users.noreply.github.com>
2024-07-11 13:51:03 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+} 
